4d3dbc16af962ab228378ed6a8630db8d57f250d
MaxSignal <maxsignalqq@gmail.com>
2022-12-20 22:43:27 +0900

    fix sdl image build

--- build.sh
+++ build.sh
@@ -131,8 +131,8 @@ cd ../
 echo "BUILDING SDL_IMAGE..."
 cd SDL_image
 chmod +x configure
-./configure --prefix=$CEGCC/arm-mingw32ce --host=arm-mingw32ce --target=arm-mingw32ce --disable-shared --disable-sdltest --enable-jpg-shared=no \
-            --enable-png-shared=no --enable-tif-shared=no --enable-webp-shared=no CFLAGS="-I$CEGCC/arm-mingw32ce/include" 
+./autogen.sh --prefix=$CEGCC/arm-mingw32ce --host=arm-mingw32ce --target=arm-mingw32ce --disable-shared --disable-sdltest --enable-jpg-shared=no \
+            --enable-png-shared=no --enable-tif-shared=no --enable-webp-shared=no
 make && make install 
 cd ../
 
